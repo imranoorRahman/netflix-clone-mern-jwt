50d8237bd06b14f1acdef70ce1daddfae520407a
Imran Rahman <mrimranoor@gmail.com>
2021-11-28 21:32:12 +0000

    register & login routes & store hash of password using secret key & remove password from login response

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,18 +1,41 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const { findOne } = require("../models/User");
 
+// REGISTER
 router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        // password: req.body.password
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.REACT_APP_SECRET_KEY).toString()
     })
 
     try{
         const user = await newUser.save();
-        res.status(201).json(user);
+        return res.status(201).json(user);
     }catch(err){
-        res.status(500).json(err);
+        return res.status(500).json(err);
+    }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+        if(!user) return res.status(401).json({message: "Incorrect Username or Password!"});
+
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.REACT_APP_SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        if(originalPassword !== req.body.password) return res.status(401).json({message: "Incorrect Username or Password!"});
+
+        const {password, ...userData} = user._doc;
+        return res.status(201).json(userData);
+
+    }catch(err){
+        return res.status(500).json(err);
     }
 });
 
