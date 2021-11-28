74509ef5042ff1b932cd7053dc40894543a0372e
Imran Rahman <mrimranoor@gmail.com>
2021-11-28 21:08:49 +0000

    routes setup

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
 
 const app = express();
-dotenv.config();
 const PORT = process.env.REACT_APP_PORT || 5000;
 
 mongoose.connect(process.env.REACT_APP_MONGO_URI, {})
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.log(err));
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
     console.log(`Backend server is live on port: ${PORT}`)
