39216ed8b434f6e01d7540d80e530d2649249280
Imran Rahman <mrimranoor@gmail.com>
2021-12-08 15:27:33 +0000

    cycle through listitems in row using arrows

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { ListItem } from "../index";
 import {
   ArrowForwardIosOutlined,
@@ -6,12 +7,26 @@ import {
 import "./list.scss";
 
 export default function List() {
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left") {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    } else if (direction === "right") {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowForwardIosOutlined />
-        <div className="container">
+        <ArrowBackIosOutlined
+          className="arrow left"
+          onClick={() => handleClick("left")}
+        />
+        <div className="container" ref={listRef}>
           <ListItem />
           <ListItem />
           <ListItem />
@@ -28,7 +43,10 @@ export default function List() {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowBackIosOutlined />
+        <ArrowForwardIosOutlined
+          className="arrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
